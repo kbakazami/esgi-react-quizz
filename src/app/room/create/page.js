e7c37f02cbcf3094b8695838d68d0178d9c1be7f
Sofia Chaudhry <sofia.chaudhry2606@gmail.com>
2024-02-07 18:32:32 +0100

    feature/RQ-3 - Gérer la création d'une partie (privée/publique)
    - Added handler to get room parameters

--- src/app/room/create/page.js
+++ src/app/room/create/page.js
@@ -1,7 +1,39 @@
+"use client";
+
 import CustomRadioWrapper from "@/components/custom-radio-wrapper";
 import CustomRadio from "@/components/custom-radio";
+import { useForm, SubmitHandler } from "react-hook-form"
+import {io as ClientIO} from "socket.io-client";
+import {useEffect, useState} from "react";
 
 export default function CreateRoom() {
+    const [socket, setSocket] = useState(null);
+    const [isConnected, setIsConnected] = useState(false);
+    const [room, setRoom] = useState(false);
+
+    useEffect(() => {
+        const socketInstance = new ClientIO({
+            path: '/api/socket/io',
+            addTrailingSlash: false,
+        }, process.env.NEXT_PUBLIC_SITE_URL);
+
+        socketInstance.on('connect', () => {
+            setIsConnected(true);
+            console.log('client connected');
+        });
+
+        socketInstance.on('disconnect', () => {
+            setIsConnected(false);
+            console.log('client disconnected');
+        });
+
+        setSocket(socketInstance);
+
+        return () => {
+            socketInstance.disconnect();
+        };
+    },[]);
+
     const roomType = [
         {id: 'private', value: 'Privée', name: 'type'},
         {id: 'public', value: 'Publique', name: 'type'},
@@ -13,49 +45,121 @@ export default function CreateRoom() {
         {id: 'hard', value: 'Difficile', name: 'difficulty'}
     ];
 
-    const categories = [];
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
 
-    for (let i = 0; i <= 20; i++) {
-        categories.push({id: `category-${i}`, value: 'Lorem Ipsum', name: 'category'})
+    const onSubmit = async (data) => {
+        const response = await fetch('/api/socket/room', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
+
+        socket.on('create-room', (data) => {
+            setRoom(data);
+            console.log('create-room', room);
+        });
     }
 
     return (
         <div>
             <h1>Création d'une salle</h1>
-            <div className={"flex flex-col mt-10"}>
-                <div className={"flex flex-row"}>
-                    <div className={"flex flex-col gap-y-6 w-1/2 justify-center"}>
-                        <div className={"flex flex-row items-center gap-x-10"}>
-                            <p className={"w-52 text-xl"}>Type de salle</p>
-                            <div className={"flex flex-row gap-x-6"}>
-                                <CustomRadioWrapper classUl={'room-type'} data={roomType}/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className={"flex flex-col mt-10"}>
+                    <div className={"flex flex-row"}>
+                        <div className={"flex flex-col gap-y-6 w-1/2 justify-center"}>
+                            <div className={"flex flex-row items-center gap-x-10"}>
+                                <p className={"w-52 text-xl"}>Type de salle</p>
+                                <div className={"flex flex-row gap-x-6"}>
+                                    <ul className={`flex flex-row gap-x-6 room-type`}>
+                                        <li>
+                                            <input {...register('type', { required: true })} type={"radio"}
+                                                   id={'private'}
+                                                   className={"btn primary"}
+                                                   value={'private'}/>
+                                            <label htmlFor={'private'}>Privée</label>
+                                        </li>
+                                        <li>
+                                            <input {...register('type', { required: true })} type={"radio"}
+                                                   id={'public'}
+                                                   className={"btn primary"}
+                                                   value={'public'}/>
+                                            <label htmlFor={'public'}>Publique</label>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className={"flex flex-row items-center gap-x-10"}>
+                                <p className={"w-52 text-xl"}>Difficulté du quizz</p>
+                                <ul className={`flex flex-row gap-x-6 difficulty`}>
+                                    <li>
+                                        <input {...register('difficulty', { required: true })} type={"radio"}
+                                               id={'easy'}
+                                               className={"btn primary"}
+                                               value={'easy'}/>
+                                        <label htmlFor={'easy'}>Facile</label>
+                                    </li>
+                                    <li>
+                                        <input {...register('difficulty', { required: true })} type={"radio"}
+                                               id={'medium'}
+                                               className={"btn primary"}
+                                               value={'medium'}/>
+                                        <label htmlFor={'medium'}>Moyen</label>
+                                    </li>
+                                    <li>
+                                        <input {...register('difficulty', { required: true })} type={"radio"}
+                                               id={'hard'}
+                                               className={"btn primary"}
+                                               value={'hard'}/>
+                                        <label htmlFor={'hard'}>Difficile</label>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                        <div className={"flex flex-row items-center gap-x-10"}>
-                            <p className={"w-52 text-xl"}>Difficulté du quizz</p>
-                            <CustomRadioWrapper classUl={'difficulty'} data={difficulties}/>
+                        <div className={"flex flex-col gap-y-6 w-1/2"}>
+                            <div className={"flex flex-row items-center gap-x-10"}>
+                                <p className={"w-52 text-xl"}>Nombre de round</p>
+                                <input {...register("roundNumber", { required: true })} type={"number"} placeholder={"Exemple : 1"} min={1}/>
+                            </div>
+                            <div className={"flex flex-row items-center gap-x-10"}>
+                                <p className={"w-52 text-xl"}>Nombre de question</p>
+                                <input {...register("questionNumber", { required: true })} type={"number"} placeholder={"Exemple : 12"} min={1}/>
+                            </div>
                         </div>
                     </div>
-                    <div className={"flex flex-col gap-y-6 w-1/2"}>
-                        <div className={"flex flex-row items-center gap-x-10"}>
-                            <p className={"w-52 text-xl"}>Type de salle</p>
-                            <input type={"number"} placeholder={"Exemple : 1"} min={1}/>
-                        </div>
-                        <div className={"flex flex-row items-center gap-x-10"}>
-                            <p className={"w-52 text-xl"}>Nombre de question</p>
-                            <input type={"number"} placeholder={"Exemple : 12"} min={1}/>
-                        </div>
+                    <div className={"flex flex-col items-center gap-y-5 mt-14"}>
+                        <p className={"text-xl"}>Choix de la catégorie</p>
+                        <ul className={"category"}>
+                            <li>
+                                <input {...register('category', { required: true })} type={"radio"}
+                                       id={'random'}
+                                       className={"btn primary"}
+                                       value={'random'}/>
+                                <label htmlFor={'random'}>Aléatoire</label>
+                            </li>
+                        </ul>
+                        <ul className={"category grid grid-cols-10 gap-x-6 gap-y-4 justify-center"}>
+                            {[...Array(20)].map((x, i) =>
+                                <li className={`category`} key={`category-${i}`}>
+                                    <input {...register('category', { required: true })} type={"radio"}
+                                           id={`category-${i}`}
+                                           className={"btn primary"}
+                                           value={`category-${i}`}/>
+                                    <label htmlFor={`category-${i}`}>Catégorie - {i}</label>
+                                </li>
+                            )}
+
+                        </ul>
                     </div>
+                    <button type={'submit'} className={"btn primary w-1/6 mt-8 mx-auto"}>Créer</button>
                 </div>
-                <div className={"flex flex-col items-center gap-y-5 mt-14"}>
-                    <p className={"text-xl"}>Choix de la catégorie</p>
-                    <ul className={"category"}>
-                        <CustomRadio id={'random'} value={"Aléatoire"} name={"category"} classLi={'category'}/>
-                    </ul>
-                    <CustomRadioWrapper classUl={'category grid grid-cols-10 gap-y-4 justify-center'} data={categories} classLi={'category'}/>
-                </div>
-                <button className={"btn primary w-1/6 mt-8 mx-auto"}>Créer</button>
-            </div>
+            </form>
         </div>
     )
 }
