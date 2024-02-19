2489fa165ec83e4365693cc23b3ec813bac6e83c
Sofia Chaudhry <sofia.chaudhry2606@gmail.com>
2024-02-19 22:39:08 +0100

    feature/RQ-3 - Gérer la création d'une partie (privée/public)
    - Prepare function for openAI

--- server/services/openai.js
+++ server/services/openai.js
@@ -4,16 +4,43 @@ const openai = new OpenAI({
     apiKey: 'chatgpt-key',
 });
 
-export default async function createQuestions(room) {
+export async function createQuestions(room) {
+    const difficulty = translateDifficulty(room.difficulty);
+
     const stream = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages: [
             {
                 role: "user",
-                content: `Peux-tu me créer ${room.questionNumber} questions sur le thème suivant : ${room.category} en mode ${room.difficulty} avec 4 propositions, la réponse et une explication pour chaque question.`,
+                content: `Peux-tu me créer ${room.questionNumber} questions sur le thème suivant : ${room.category} en mode ${difficulty} avec 4 propositions, la réponse et une explication pour chaque question.`,
             }
         ],
         stream: true,
     });
     console.log(stream);
+}
+
+export async function generateTopic() {
+    const stream = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: "user",
+                content: `Peux-tu me créer 20 sujets différents pour des questions.`,
+            }
+        ],
+        stream: true,
+    });
+    console.log(stream);
+}
+
+function translateDifficulty(difficulty) {
+    switch(difficulty) {
+        case 'easy':
+            return 'Facile';
+        case 'medium':
+            return 'Moyen';
+        case 'hard':
+            return 'Difficile';
+    }
 }
